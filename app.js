a540e4ad04bbfb0a9b6bf2e8a57c1a6ddfdde583
Joselio-Blip <125415221+Joselio-Blip@users.noreply.github.com>
2023-04-19 15:42:29 +0200

    Done

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
 const app = express();
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -15,10 +16,31 @@ const movieHandlers = require("./movieHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
+const { getUsers, getUserById } = require("./userHandlers");
+
+app.get("/api/users", getUsers);
+app.get("/api/users/:id", getUserById);
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+pool
+  .getConnection()
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(port, (err) => {
+      if (err) {
+        console.error("Something bad happened");
+      } else {
+        console.log(`Server is listening on ${port}`);
+      }
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ userHandlers.js
@@ -0,0 +1,43 @@
+const mysql = require("mysql2/promise");
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+const getUsers = (req, res) => {
+  pool
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
