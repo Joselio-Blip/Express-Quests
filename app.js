3ca4d46dbcc3bea27b862c14a91edf864e51f249
Joselio-Blip <125415221+Joselio-Blip@users.noreply.github.com>
2023-05-15 07:53:09 +0200

    Express 05

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 
 const userHandlers = require("./userHandlers");
@@ -28,6 +29,7 @@ app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUser);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 const { validateMovie } = require("./validators.js");
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
