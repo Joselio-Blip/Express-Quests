2890633093cd46d8c34074cef3d790d1c2d9eacc
Joselio-Blip <125415221+Joselio-Blip@users.noreply.github.com>
2023-05-15 04:54:48 +0200

    Update movieHandlers.js

--- movieHandlers.js
+++ movieHandlers.js
@@ -28,41 +28,14 @@ const movies = [
 const database = require("./database");
 
 const getMovies = (req, res) => {
-  res.json(movies);
-};
-
-const postUsers = (req, res) => {
-  const { firstname, lastname, email, city, language } = req.body;
-
   database
-  .query(
-    "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-    [firstname, lastname, email, city, language]
-  )
-  .then(([result]) => {
-    res.location(`/api/users/${result.insertId}`).sendStatus(201);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error saving the user");
-  });
-};
-
-
-  const postMovie = (req, res) => {
-  const { title, director, year, color, duration } = req.body;
-
-  database
-    .query(
-      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
-      [title, director, year, color, duration]
-    )
-    .then(([result]) => {
-      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Error saving the movie");
+      res.status(500).send("Error retrieving data from database");
     });
 };
 
@@ -84,9 +57,52 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
+// in movieHandlers.js
+
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the movie");
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
-  postMovie, 
-  postUsers
+  postMovie,
+  updateMovie, // don't forget to export your function ;)
 };
+
+
