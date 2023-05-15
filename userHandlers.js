34082d4475f060e7f5823ba4072db4a7c5692b08
Joselio-Blip <125415221+Joselio-Blip@users.noreply.github.com>
2023-05-15 04:55:13 +0200

    Update userHandlers.js

--- userHandlers.js
+++ userHandlers.js
@@ -1,14 +1,7 @@
-const mysql = require("mysql2/promise");
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+const database = require("./database");
 
 const getUsers = (req, res) => {
-  pool
+  database
     .query("select * from users")
     .then(([users]) => {
       res.json(users);
@@ -19,10 +12,30 @@ const getUsers = (req, res) => {
     });
 };
 
+// ...
+
+const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+  
+    database
+      .query(
+        "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+        [firstname, lastname, email, city, language]
+      )
+      .then(([result]) => {
+        res.location(`/api/users/${result.insertId}`).sendStatus(201);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error saving the user");
+      });
+  };
+  
+ 
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  pool
+  database
     .query("select * from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
@@ -37,7 +50,31 @@ const getUserById = (req, res) => {
     });
 };
 
-module.exports = {
-  getUsers,
-  getUserById,
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
 };
+
+module.exports = {
+    getUsers,
+    getUserById,
+    postUser,
+    updateUser,
+  };
